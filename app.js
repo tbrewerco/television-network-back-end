0e82d322f5689070f7fafdde374e814e47bf30b9
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2022-12-20 18:14:17 -0700

    add routes to app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 import express from 'express';
-const app = express();
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import networkRoutes from './routes/networkRoutes.js';
+import packageRoutes from './routes/packageRoutes.js';
+import showRoutes from './routes/showRoutes.js';
 
+const app = express();
+
+// enable cors for all routes
+app.use(cors());
+
+// parse incoming request bodies 
 app.use(bodyParser.json());
 
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
-
-app.get('/', (req, res) => {
-    res.json({ info: 'test' })
-})
+// routes
+app.use('/networks', networkRoutes);
+// app.use('/packages', packageRoutes);
+// app.use('/shows', showRoutes);
 
 export default app;
